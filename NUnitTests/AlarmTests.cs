9e38222a22bd72d18af4c14952f0ee012e9bb4bd
Mahir Korjenić <mahir.korjenic@edu.fit.ba>
2022-08-16 17:24:14 +0200

    Installed Moq plugin for mocks for tests. Created Check_ExpectedValues where check expected result. I mock 2 interfaces and send it to method for test.

--- NUnitTests/AlarmTests.cs
+++ NUnitTests/AlarmTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
+using TDDMicroExercises.Interfaces;
 using TDDMicroExercises.TirePressureMonitoringSystem;
 
 namespace NUnitTests
@@ -6,6 +8,9 @@ namespace NUnitTests
     [TestFixture]
     public class AlarmTests
     {
+        private readonly Mock<ISensor> _mockSensor = new Mock<ISensor>();
+        private readonly Mock<IAlarmPressureTreshold> _mockPressureTreshold = new Mock<IAlarmPressureTreshold>();
+
         [Test]
         public void Check_AlarmOn_DefaultValue_ShouldBeFalse()
         {
@@ -14,6 +19,28 @@ namespace NUnitTests
 
             //Assert
             Assert.IsFalse(alarm.AlarmOn);
+        }
+
+        [Test]
+        public void Check_ExpectedValues()
+        {
+            // Arrange
+            double sensorValue = 5;
+            double highPressure = 1;
+            double lowPressure = 1;
+
+            _mockSensor.Setup( sensor => sensor.PopNextPressurePsiValue()).Returns(sensorValue);
+
+            _mockPressureTreshold.Setup( pressure => pressure.HighPressureThreshold).Returns(highPressure);
+            _mockPressureTreshold.Setup( pressure => pressure.LowPressureThreshold).Returns(lowPressure);
+
+            IAlarm alarm = new Alarm(_mockSensor.Object, _mockPressureTreshold.Object);
+
+            // Act
+            alarm.Check();
+
+            //Assert
+            Assert.IsTrue(alarm.AlarmOn);
         }
     }
 }
