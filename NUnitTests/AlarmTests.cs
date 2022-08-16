1cea435ad087598835ccf58e44ce86cf75125afa
Mahir Korjenić <mahir.korjenic@edu.fit.ba>
2022-08-16 17:08:49 +0200

    Create 1st test to check default value.

---
+++ NUnitTests/AlarmTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TDDMicroExercises.TirePressureMonitoringSystem;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class AlarmTests
+    {
+        [Test]
+        public void Check_AlarmOn_DefaultValue_ShouldBeFalse()
+        {
+            // Arrange & Act
+            var alarm = new Alarm();
+
+            //Assert
+            Assert.IsFalse(alarm.AlarmOn);
+        }
+    }
+}
